290dc1358c5fd9c2f7024fd2d52fc26658475eb4
Corey Thompson <corey.dwayne.thompson@gmail.com>
2019-09-25 00:40:44 -0500

    first copy from oas.com

--- web/src/routes/about.svelte
+++ web/src/routes/about.svelte
@@ -1,7 +1,120 @@
 <svelte:head>
 	<title>About</title>
 </svelte:head>
+<h1>About OAS</h1>
 
-<h1>About this site</h1>
+<p>
+Orange Alligator Solutions brings the technology and efficiencies of large companies to smaller local businesses, helping them and their communities grow. We believe that better local business leads to better lives for everyone in those communities.
+</p>
 
-<p>This is the 'about' page. There's not much here.</p>
+<h2>What is OAS?</h2>
+
+<p>
+OAS offers business owners the benefit of dealing with a single point of contact to manage any project that entails expanding their business. We are primarily a small software development firm in Tulsa, OK, backed by a network of specialized consultants, allowing us to fulfill any needs of growing businesses.
+</p>
+<p>
+Often, software projects can require expertise outside of the realm of just code. As an owner, you may know that you want to control a machine with your phone, but you don’t have the time to perform research, hire an expert in mobile development, hire an expert in mechanical engineering and coordinate the project. Automation often requires mechanical specialists, websites often require graphics, branding and social media specialists and data visualization often requires IT and graphic specialists.
+</p>
+<p>
+We have crafted a network to ensure each project is completed on time with exceptional quality by filling key roles with experts in their domains. Project management, consultation with these subject-matter experts and their coordination with the software is handled by OAS, keeping you the business owner available to remain productive
+</p>
+
+<h2>Meet Our Experts</h2>
+
+	<!-- wpshopmart team builder wrapper -->
+	<div class="wpsm_team_2_b_row" id="wpsm_team_2_b_row_4321">
+		<div class="wpsm_row">  
+			<style>
+			#wpsm_team_2_b_row_4321 .wpsm_team_2_member_wrapper_inner h3{
+				color:#000000 !important;
+				font-size:18px !important;
+				font-family:Open Sans !important;
+			}
+			
+			#wpsm_team_2_b_row_4321 .wpsm_team_2_b_desig{
+				color:#000000 !important;
+				font-size:14px !important;
+				font-family:Open Sans !important;
+			}
+			#wpsm_team_2_b_row_4321 .wpsm_team_2_b_desc{
+				color:#000000 !important;
+				font-size:14px !important;
+				font-family:Open Sans !important;
+			}
+			#wpsm_team_2_b_row_4321 .wpsm_team_2_social_div a i{
+				color:#4f4f4f !important;
+				border-color:#4f4f4f !important;
+			}
+			#wpsm_team_2_b_row_4321 .wpsm_team_2_member_wrapper{
+				background:#ffffff !important;
+			}
+						</style>
+						 
+					
+					
+					<div class="col-md-4 wpsm-col-div">
+						<div class="wpsm_team_2_member_wrapper">
+							<img class="img-responsive wpsm_team_2_mem_img" src="https://orangealligatorsolutions.com/wp-content/uploads/2019/04/Corey-chair-square500x500.jpg" alt="Corey Thompson">
+							<div class="wpsm_team_2_member_wrapper_inner">
+								<h3>
+									Corey Thompson								</h3>
+								<span class="wpsm_team_2_b_desig"> Owner, Software Engineer </span> 								<div class="wpsm_team_2_social_div">
+									<a href="https://www.facebook.com/coreydwaynethompson" target="_blank" title="facebook profile"><i class="fa fa-facebook"></i></a> 																		<a href="https://www.linkedin.com/in/coreydthompson/" target="_blank" title="linkedin profile"><i class="fa fa-linkedin"></i></a>																	</div>
+								<p class="wpsm_team_2_b_desc"> Corey is a father, web developer, Data Visualization Society contributor, electrical engineer and full-time software engineer in Tulsa, OK. He has a specialty in manufacturing and quality control software with nearly a decade of experience working within the electronics, HVAC, oil and gas industries. </p> 								
+							</div>
+						</div>
+					</div>
+					
+													 
+					
+					
+					<div class="col-md-4 wpsm-col-div">
+						<div class="wpsm_team_2_member_wrapper">
+							<img class="img-responsive wpsm_team_2_mem_img" src="https://orangealligatorsolutions.com/wp-content/uploads/2019/04/trey_bishop_team.jpg" alt="Trey Bishop">
+							<div class="wpsm_team_2_member_wrapper_inner">
+								<h3>
+									Trey Bishop								</h3>
+								<span class="wpsm_team_2_b_desig"> Designer, Innovation Consultant </span> 								<div class="wpsm_team_2_social_div">
+																											<a href="https://www.linkedin.com/in/treybishop/" target="_blank" title="linkedin profile"><i class="fa fa-linkedin"></i></a>																	</div>
+								<p class="wpsm_team_2_b_desc"> Trey is an inventor, concept artist and graphic designer with over fifteen years of experience working for creative agencies, large corporations and educational institutions. He specializes in exploring bold new ideas and innovating beyond the status quo and has worked with clients such as Circle 5 Studios (previously Lukewarm Media), Tri County Tech, ESI with Walmart Labs, Questel, Executive IP and Nemesis Studios. </p> 								
+							</div>
+						</div>
+					</div>
+					
+													 
+					
+					
+					<div class="col-md-4 wpsm-col-div">
+						<div class="wpsm_team_2_member_wrapper">
+							<img class="img-responsive wpsm_team_2_mem_img" src="https://orangealligatorsolutions.com/wp-content/uploads/2019/04/20181212_203004.jpg" alt="Blake Tucker">
+							<div class="wpsm_team_2_member_wrapper_inner">
+								<h3>
+									Blake Tucker								</h3>
+								<span class="wpsm_team_2_b_desig"> Network Analyst, Technology Consultant </span> 								<div class="wpsm_team_2_social_div">
+																											<a href="https://www.linkedin.com/in/blake-t-85109783/" target="_blank" title="linkedin profile"><i class="fa fa-linkedin"></i></a>																	</div>
+								<p class="wpsm_team_2_b_desc"> Blake is a Network and Systems Analyst with over half a decade in the field. He uses his expertise in Azure's cloud services and Office 365 to help clients meet their Information Technology needs. He has worked in many industries such as oil and gas, healthcare, manufacturing, sales, marketing and legal. </p> 								
+							</div>
+						</div>
+					</div>
+					
+											</div>
+						<div class="wpsm_row">
+														 
+					
+					
+					<div class="col-md-4 wpsm-col-div">
+						<div class="wpsm_team_2_member_wrapper">
+							<img class="img-responsive wpsm_team_2_mem_img" src="https://orangealligatorsolutions.com/wp-content/uploads/2019/09/profile_image.png" alt="Lee Whitehead">
+							<div class="wpsm_team_2_member_wrapper_inner">
+								<h3>
+									Lee Whitehead								</h3>
+								<span class="wpsm_team_2_b_desig"> Growth and Enablement Consultant </span> 								<div class="wpsm_team_2_social_div">
+																											<a href="https://www.linkedin.com/in/leetwhitehead/" target="_blank" title="linkedin profile"><i class="fa fa-linkedin"></i></a>																	</div>
+								<p class="wpsm_team_2_b_desc"> Lee is the owner of UVOICE Consulting, Inc. and has over 8 years of marketing, technology and sales enablement experience. He has a specialty in technology and SaaS businesses with a heavy focus on sustainable growth. Lee uses his vast experience in technology and marketing to help clients focus on growth through execution. </p> 								
+							</div>
+						</div>
+					</div>
+					
+												
+		</div>
+	</div>
