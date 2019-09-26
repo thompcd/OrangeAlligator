23ecd2766ee1273312439578438cb96c15b8c91f
Corey Thompson <corey.dwayne.thompson@gmail.com>
2019-09-26 15:17:36 -0500

    nicer looking nav with home icon

--- web/src/components/Nav.svelte
+++ web/src/components/Nav.svelte
@@ -7,6 +7,7 @@
 		border-bottom: 1px solid rgba(255,62,0,0.1);
 		font-weight: 300;
 		padding: 0 1em;
+		height: 58px;
 	}
 
 	ul {
@@ -23,7 +24,23 @@
 
 	li {
 		display: block;
+		float: right;
+		/* height: 50px; */
+	}
+
+	#home{
 		float: left;
+		/* align-self: flex-start; */
+	}
+
+	#home img{
+		height: 44px;
+		padding: 6px 0 0px 0px;
+
+	}
+
+	#home a{
+		padding: 0 0 ;
 	}
 
 	.selected {
@@ -34,27 +51,27 @@
 	.selected::after {
 		position: absolute;
 		content: '';
-		width: calc(100% - 1em);
+		width: 100%;
 		height: 2px;
 		background-color: #2e9be6;
 		display: block;
-		bottom: -1px;
+		bottom: -3px;
 	}
 
 	a {
 		text-decoration: none;
-		padding: 1em 0.5em;
+		padding: 1.9em 0.5em .1em 0.5em;
 		display: block;
 	}
 </style>
 
 <nav>
 	<ul>
-		<li><a class='{segment === undefined ? "selected" : ""}' href='.'>home</a></li>
-		<li><a class='{segment === "about" ? "selected" : ""}' href='about'>about</a></li>
+		<li class='{segment === undefined ? "selected" : ""}' id="home"><a  href='.'><img src="./oas-cog-title.png" alt="orange alligator logo"/></a></li>
 
 		<!-- for the blog link, we're using rel=prefetch so that Sapper prefetches
 		     the blog data when we hover over the link or tap it on a touchscreen -->
-		<li><a rel=prefetch class='{segment === "blog" ? "selected" : ""}' href='blog'>blog</a></li>
+		<li class='{segment === "blog" ? "selected" : ""}' ><a rel=prefetch  href='blog'>blog</a></li>
+		<li class='{segment === "about" ? "selected" : ""}' ><a rel=prefetch  href='about'>about</a></li>
 	</ul>
 </nav>
