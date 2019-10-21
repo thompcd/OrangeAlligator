070dbb3c92069831628a0a4be736262441d4a7c1
Corey Thompson <corey.dwayne.thompson@gmail.com>
2019-10-21 08:12:32 -0500

    close to finished content

--- web/src/routes/portfolio.svelte
+++ web/src/routes/portfolio.svelte
@@ -3,6 +3,7 @@ import { slide } from 'svelte/transition';
 import { cubicInOut } from 'svelte/easing';
 // import Radial from "../components/RadialGraph.svelte";
 import Cloud from "../components/Cloud.svelte";
+import Section from "../components/Section.svelte";
 
 let collapse = true;
 
@@ -11,7 +12,7 @@ let collapse = true;
     }
 </script>
 
-<div class="section hero flex">
+<div id="hero" class="section hero flex">
     <div class="hero-inner">
     <h1>Adding interactivity to ditch reports</h1>
     <h3>How we made a more usable KPI dashboard</h3>
@@ -68,9 +69,10 @@ let collapse = true;
     </ul>
 </div>
 
+
 <div class="section center-container">
     <button class="primary-btn" on:click={toggleCollapse}>
-    	{collapse === true ? '˅ Read More ˅' : '˄ Hide ˄'}
+    	{collapse === true ? 'Read Story' : 'Hide Story'}
     </button>
 </div>
 
@@ -88,94 +90,100 @@ let collapse = true;
     </div>
     
     <div class="section">
-        <h2>Existing System</h2>
-        <!-- <p>Our client prides themselves on being a metrics-driven company.
-        However, these daily metrics were buried under complex data, requiring a full report to be generated
-        </p> -->
-
-        <p>Our client prides themselves on being a metrics-driven company.
-        However, the process to distribute these values to employees seems cumbersome and frequent.
-        </p>
-    </div>
+        <h2>Existing System</h2><p>Our client prides themselves on being a metrics-driven company. However, the process to distribute these values to employees seems cumbersome and frequent.
+        <br>After a consultation, we were asked to create a better experience for the employees to access and track these values daily.</p>
+        <div class="sub"><h3>Sharepoint</h3><p>The company has their PLM system tied into Sharepoint. Sharepoint dashboard offers a hub for all employees.</p></div>
+        
+     </div>
 
     <div class="section">
         <h2>Identifying What to Fix</h2>
-        <div class="center-container flex wrap">
+        <div id="fixes" class="center-container wrap">
             <div class="one-third"><h3>Ease of Access</h3><p>Employees use these values in their everyday workflow, they should be readily available.</p></div>
             <div class="one-third"><h3>Poor Usability</h3><p>Data existis in a complex report. A user has to traverse too much data for their use-case.</p></div>
             <div class="one-third"><h3>High Barrier</h3><p>A complex report prevents new employees from understanding the numbers governing their work.</p></div>
         </div>
     </div>
 
-    <div class="section"><h2>Sharepoint
-    <p>The company has their PLM system tied into Sharepoint. Sharepoint dashboard offers a hub for all employees.</p>
-    </h2></div>
-    
-    <div class="section"><h2>Interactive Dashboard Widgets</h2><p>Charts exist to provide digest information of large datasets at the sacrifice of granularity of data. Using interactive charts, we can retain the granularity of information on the client's KPI's.</p></div>
-
-    <div class="section"><h2>Macro/Micro</h2><p>A measurement isn't valuable on its own, the numbers are only relevant in comparison to others. We had the concept of a macro and micro pair of charts to quickly understand today's numbers and the sources that drive that them as well vs. the trends of the past year.</p></div>
-
     <div class="section">
-        <h2>Existing System</h2>
-        <p>We find the KPI calculations and trace the sources to identify the minimum amount of data to required.
-        </p>
-        <div class="center-container">
-            <img class="shadow" src="./blank-sharepoint-example.jpg" alt="Stock Sharepoint dashboard">
-        </div>
-    </div>
-    <div class="section">
-        <h2>Existing System</h2>
-        <p>From the data structure uncovered, we recommended potential charts that could offer both results at a glimpse and interactivity to allow a user to immediately dig deeper into any questionable results.
-        </p>
-        <div class="center-container">
-            <img class="shadow" src="./blank-sharepoint-example.jpg" alt="Stock Sharepoint dashboard">
-        </div>
+        <h2>First Thoughts</h2><p>After some research and thought about the issues, we proposed a concept.</p>
+        <div class="sub"><h3>Interactive Dashboard Widgets</h3><p>Charts exist to provide digest information of large datasets at the sacrifice of granularity of data. Using interactive charts, we can retain the granularity of information on the client's KPI's.</p></div>
+        <div class="sub"><h3>Macro/Micro</h3><p>A measurement isn't valuable on its own, the numbers are only relevant in comparison to others. We had the an idea of a macro and micro pair of widgets to quickly understand today's numbers and the sources that drive that them as well vs. the trends of the past year.</p></div>
     </div>
 
     <div class="section">
-        <h2>The Existing</h2>
-        <div class="side magnifying-glass">
-            <p class="two-thirds">We find the KPI calculations and trace the sources to identify the minimum amount of data to required.</p>
+        <h2>Laying a Path</h2><p>With the buy-in of the client, we perform research to cement a design.</p>
+        <div class="sub"><h3>Sharepoint Framework (SPFx.js)</h3><p>This library has been proven enterprise production-ready and allows us to use the power of javascript within Sharepoint. Using Sharepoint Server 2016 puts us in the range of official support.</p></div>
+        <div class="sub"><h3>D3.js</h3><p>When it comes to interactive charts in javascript, D3 is always in the conversation. We opted to use v4 for some IE11 guarantees.</p></div>
+        <div class="sub"><h3>Sharepoint Apps</h3><p>Our code can be bundled into a sharepoint app and shipped via the client's local app store. Again, Sharepoint Server 2016 is within range of official support.</p></div>
+    </div>
+
+    <section>
+        <h2>First Pass</h2><p>With approval, we started development.</p>
+        <div class="sub"><h3>Sunburst Chart</h3>
+            <div class="side">
+                <div class="two-thirds">
+                    <p>Our datasets had hierarchical qualitative categories each with a quantitative value.
+                    <br>We let the data lead us to the chart and the sunburst is a great fit for uncovering our strucutre.
+                    These charts are very flashy and are often used in a sub-optimal capacity, so when we chose it we did some soul-searching to make sure we weren't led astray.
+                    <br>Ultimately, we felt confident in our method of selection. We used this as our micro chart, displaying month-to-date values.
+                    <br>To make better use of space, we hollowed the center and added an "always-on" indicator, so a user could get exactly what they needed without even using the chart if they needed.
+                    </p>
+                </div>
+                <div class="one-third"><img src="./radial-nohover.png" alt="Sunburst Chart"></div>
+            </div>
         </div>
 
-        <div class="side chart-icon">
-            <div class="flex one-third">
-                <p class="step">2</p>
-                <img class="step-icon" src="./ci8-dashboard-redacted-crop.jpg" alt="The given datasets had hierarchical qualitative categories each with a quantitative value.">
-                <p>From the data structure uncovered, we recommended potential charts that could offer both results at a glimpse and interactivity to allow a user to immediately dig deeper into any questionable results.</p>
+        <div class="sub"><h3>Heatmap</h3>
+            <div class="side">
+                <div class="one-third"><img src="./heatmap.png" alt="Heatmap"></div>
+                <div class="two-thirds">
+                    <p>While looking at our data, we had even more qualitative fields available. We really wanted our macro chart to complement in more than just theory, choosing the proper chart was crucial to make this happen.
+                    <br>Rather than viewing multiple lines or clusters of histograms, we were able to utilize a heatmap to display a rolling 12 month window of monthly bins, with an added category breakdown.
+                    <br>Another bonus to this choice was, knowing we wanted to add interactivity, it is much more usable than its competitors. The square bins are much easier to navigate and display tooltips than points or lines on hover.
+                    </p>
+                </div>
             </div>
-            <p class="two-thirds"> </p>
         </div>
 
-        <div class="side mail-icon">
-            <div class="flex one-third">
-                <p class="step">3</p>
-                <p>Emails, calls and revision.</p>
-            </div>
-            <p class="two-thirds">The given datasets had hierarchical qualitative categories each with a quantitative value</p>
-        </div>
+    </section>
 
     <div class="section">
-        <h2>Challenges</h2>
+        <h2>Adding Interactivity For Ultimate Value</h2>
+
+        <div class="sub"><h3>Heatmap</h3>
+                <div class="side">
+                    <div class="two-thirds">
+                        <p>By adding a tooltip box with the context of that month's metrics, the user gained so much power to utilize the numbers at a glance or dig deep to investigate the source of the numbers, all immediately available to the employee.</p>
+                    </div>
+                    <div class="one-third"><img src="./radial-hover-stock.png" alt="Sunburst hover"></div>
+                </div>
+        </div>
+        <div class="sub"><h3>Sunburst</h3>
+                <div class="side">
+                    <div class="one-third"><img src="./heatmap-hover.png" alt="Heatmap hover"></div>
+                    <div class="two-thirds">
+                        <p>The center indicator now updates with the specific values of the hovered item, replacing the existing indicator and informing the user through design that they have left a high-level context and entered a new one.</p>
+                    </div>
+                </div>
+        </div>
+    </div>
+
+    <div class="section">
+        <h2>Retrospective</h2>
+        <h3>Biggest Challenges</h3>
         <ul>
-            <li>Need context of entire report to gain an understanding of the current state of metrics</li>
-            <li>Retaining granularity of report data without the clutter</li>
+            <li>Finding the balance between granularity of data vs clutter</li>
             <li>Refresh rate of Sharepoint lists as data source</li>
             <li>Limitations of Sharepoint Framework and web parts within Sharepoint 2016</li>
             <li>Support for IE11</li>
         </ul>
     </div>
 
-        <div class="section">
-            <h2>Playlist</h2>
-            <a href="https://open.spotify.com/playlist/1t87DF4KAj9Vuz9QILyCQP?si=8Cl9kroHSl6DJthG-yhv4w">🎵 Acoustic Rock - stevenoss 🎵</a>
-        </div>
-
-        <div class="section center-container">
-    <button class="primary-btn" on:click={toggleCollapse}>
-    	{collapse === true ? '˅ Read More ˅' : '˄ Hide ˄'}
-    </button>
-</div>
+    <div class="section center-container">
+        <button class="primary-btn" on:click={toggleCollapse}>
+            {collapse === true ? 'Read Story' : 'Hide Story'}
+        </button>
     </div>
 </div>
 {/if}
@@ -191,8 +199,7 @@ let collapse = true;
     height: 2rem;
 }
 .primary-btn:hover{
-    color: #2e9be6;
-    
+    border: solid 2px #2e9be6;
 }
 .align{
     text-align: center;
@@ -276,6 +283,10 @@ let collapse = true;
     margin: auto;
 }
 
+.one-third img{
+    width: 100%;
+}
+
 /* mobile */
 @media only screen and (max-width: 600px) {
     .hero{
@@ -297,12 +308,24 @@ let collapse = true;
         z-index: 5;
     }
 
+    .sub{
+        margin-bottom: 0.5rem;
+        z-index: 5;
+    }
+
     .one-fourth{
         text-align: center;
         justify-content: center;
     }
 
     .one-third{
+        flex-wrap: wrap;
+    }
+
+    #fixes{
+        display: block;
+    }
+    .two-thirds{
         flex-wrap: wrap;
     }
     .note{
@@ -313,6 +336,10 @@ let collapse = true;
 
 /* desktop */
 @media only screen and (min-width: 600px) {
+
+    #hero{
+        margin-bottom: 2rem;
+    }
     .hero{
         width: 100%;
         height: 250px;
@@ -335,7 +362,11 @@ let collapse = true;
     }
 
     .section{
-        margin-bottom: 2rem;
+        margin-bottom: 8rem;
+        z-index: 5;
+    }
+    .sub{
+        margin-bottom: 4rem;
         z-index: 5;
     }
 
@@ -348,6 +379,11 @@ let collapse = true;
 
     .one-third{
         width: 33%;
+        padding: 0.5rem 2rem;
+    }
+
+    .two-thirds{
+        width: 67%;
         padding: 0.5rem 2rem;
     }
     
@@ -398,10 +434,6 @@ let collapse = true;
     height: 100px;
 }
 
-.two-thirds{
-    width: 67%;
-    padding: 0.5rem 2rem;
-}
 .center-container{
     display: flex;
     align-content: center;
