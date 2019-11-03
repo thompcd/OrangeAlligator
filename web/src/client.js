10ccd1d72564608db1b03877333de027aa9b111a
Corey Thompson <corey.dwayne.thompson@gmail.com>
2019-11-03 14:50:08 -0600

    add logrocket

--- web/src/client.js
+++ web/src/client.js
@@ -1,4 +1,7 @@
 import * as sapper from '@sapper/app';
+import LogRocket from 'logrocket';
+
+LogRocket.init('vx3sp9/orangealligatorio-prod');
 
 sapper.start({
 	target: document.querySelector('#sapper')
